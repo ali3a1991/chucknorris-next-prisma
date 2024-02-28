0b409dffce76439cd63dd9e880695063d4925872
Your Name <ali3ajob@gmail.com>
2024-02-28 15:44:16 +0100

    visited debuging

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,29 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getVisitor, setVisitor } from "@/lib/visitorPrisma";
 
 function Footer() {
   const [visitedNum, setVisitedNum] = useState(0);
-  const visited = sessionStorage.getItem("visited");
 
-  if (visited) {
-    getVisitor().then((value) => {
-      if (value) {
-        setVisitedNum(value);
-      }
-    });
-  } else {
-    sessionStorage.setItem("visited", "true");
-    setVisitor().then((value) => {
-      if (value) {
-        setVisitedNum(value);
-      }
-    });
-  }
+  useEffect(() => {
+    const visited = sessionStorage.getItem("visited");
+
+    if (visited) {
+      getVisitor().then((value) => {
+        if (value) {
+          setVisitedNum(value);
+        }
+      });
+    } else {
+      sessionStorage.setItem("visited", "true");
+      setVisitor().then((value) => {
+        if (value) {
+          setVisitedNum(value);
+        }
+      });
+    }
+  }, [])
   
   return (
     <footer className="flex h-[100px] items-center px-12 rounded-t-3xl bg-gray-300 font-bold text-3xl">
